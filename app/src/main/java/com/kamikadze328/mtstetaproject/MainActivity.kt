df9049d31947ca5509eef9a195d418de7a9b5f68
Sergey <48965524+kamikadze328@users.noreply.github.com>
2021-07-10 20:29:57 +0300

    completed basic task

--- app/src/main/java/com/kamikadze328/mtstetaproject/MainActivity.kt
+++ app/src/main/java/com/kamikadze328/mtstetaproject/MainActivity.kt
@@ -5,8 +5,13 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.res.ResourcesCompat
 import androidx.core.view.doOnLayout
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.appbar.AppBarLayout
+import com.kamikadze328.mtstetaproject.objects.Actor
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,15 +23,53 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_details)
 
-
+        prepareRecycleViewActors()
         initToolBar()
     }
+
+    private fun prepareRecycleViewActors() {
+        val recycler = findViewById<RecyclerView>(R.id.recycle_view_actors)
+        val actors = prepareActors()
+        val adapter = MovieActorsAdapter.ActorAdapter(actors)
+        recycler.adapter = adapter
+        recycler.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+
+        val actorsDivider =
+            ResourcesCompat.getDrawable(resources, R.drawable.movie_actors_divider, theme)!!
+        val dividerItemDecoration = DividerItemDecoration(this, LinearLayoutManager.HORIZONTAL)
+        dividerItemDecoration.setDrawable(actorsDivider)
+        recycler.addItemDecoration(dividerItemDecoration)
+    }
+
+    private fun prepareActors(): List<Actor> {
+        return listOf(
+            Actor(
+                getActorDrawableIcon(R.drawable.img_actor_statham),
+                "Джейсон Стэйтем"
+            ),
+            Actor(
+                getActorDrawableIcon(R.drawable.img_actor_mccallany),
+                "Холт Маккэллани"
+            ),
+            Actor(
+                getActorDrawableIcon(R.drawable.img_actor_hartnett),
+                "Джош Харнетт"
+            )
+        )
+    }
+
+    private fun getActorDrawableIcon(id: Int) =
+        ResourcesCompat.getDrawable(resources, id, theme) ?: ResourcesCompat.getDrawable(
+            resources,
+            R.drawable.ic_baseline_image_not_supported_24,
+            theme
+        )
 
 
     private fun initToolBar() {
         toolbar = findViewById(R.id.toolbar)
-        toolbarBeforeCollapsed = findViewById(R.id.toolbarBeforeCollapsed)
-        val appBarLayout = findViewById<AppBarLayout>(R.id.appBarLayout)
+        toolbarBeforeCollapsed = findViewById(R.id.toolbar_before_collapsed)
+        val appBarLayout = findViewById<AppBarLayout>(R.id.app_bar_layout)
         toolbar.doOnLayout {
             scrollRange = appBarLayout.totalScrollRange
         }
